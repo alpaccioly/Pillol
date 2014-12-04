eb847e6205d0ac1610ef2e74746fc874864a1312
Arthur <alpa@cin.ufpe.br>
2014-12-03 21:23:42 -0300

    BoardSystem

---
+++ Assets/Project/Scripts/Game/Board/BoardBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Board
+{
+	public class BoardBehaviour : MonoBehaviour 
+	{
+		private int sizeX = 10;
+		private int sizeY = 5;
+
+		public PointBoard? GetPositionBoard(Vector3 point)
+		{
+			Vector3 scale = transform.Find ("Floor").transform.localScale;
+			Vector3 origin = new Vector3(-scale.x/2,0,-scale.z/2);
+			Vector3 rel = (point - origin);
+			PointBoard p = new PointBoard ();
+			p.I = (int)((rel.x / scale.x) * sizeX);
+			p.J = (int)((rel.z / scale.z) * sizeY);
+			return p;
+		}
+	}
+	public struct PointBoard
+	{
+		public int I;
+		public int J;
+	}
+}
---
+++ Assets/Project/Scripts/Game/CubeBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using Board;
+
+public class CubeBehaviour : MonoBehaviour {
+
+	public BoardBehaviour BoardScript; 
+
+	void Awake()
+	{
+		BoardScript = transform.parent.parent.GetComponent<BoardBehaviour> ();
+	}
+	// Update is called once per frame
+	void Update () {
+		PointBoard p = (PointBoard)BoardScript.GetPositionBoard (transform.localPosition);
+		Debug.Log (string.Format("{0}, {1}",p.I, p.J));
+		       
+	}
+}
