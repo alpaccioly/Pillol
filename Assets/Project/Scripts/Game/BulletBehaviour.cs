330fb12d3aeee562a00bdf0387b7dd6db02d3ad0
arthurmnd <arbmendonca@gmail.com>
2015-01-27 11:33:48 -0300

    Criados os prefabs e behaviours de Bullet e Character.

---
+++ Assets/Project/Scripts/Game/BulletBehaviour.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletBehaviour : MonoBehaviour {
+	private Vector3 position, target;
+	private float speed = 5.0f;
+	private float h = 3.0f;
+	private double damage; //CALCULAR DE ACORDO COM PROPRIEDADES
+	private float tmax;
+	private bool strt;
+	private float t;
+
+	// Use this for initialization
+	void Awake () {
+		t = 0.0f;
+	}
+	void Start () {
+	
+	}
+	public void MoveTo (Vector3 posicaoAlvo){
+		target = posicaoAlvo;
+		float d = Vector3.Distance (position, target);
+		tmax = d / speed;
+		strt = true;
+	}
+	void ChangePosition (float t){
+		position = (1 - t) * position + t * target;
+		position.z = 4 * h * (t - Mathf.Pow (t, 2));
+	}
+	void OnTriggerEnter (Collider col){
+		Debug.Log ("Colidiu");
+		CharacterBehaviour ch = col.transform.GetComponent<CharacterBehaviour>();
+		ch.Hit (damage);
+		Destroy (gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (strt) {
+			t = t + Time.deltaTime;
+			ChangePosition(t/tmax);	
+		}
+	
+	}
+}
---
+++ Assets/Project/Scripts/Game/CharacterBehaviour.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterBehaviour : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	void Attack (Transform alvo) {
+		GameObject go = (GameObject)Instantiate(Resources.Load("Bullet"));
+		go.transform.parent = transform.parent.parent;
+		transform.localPosition = Vector3.zero;
+		transform.localScale = Vector3.one;
+		BulletBehaviour bala = go.GetComponent<BulletBehaviour>();
+		bala.MoveTo (alvo.position);
+	}
+
+	public void Hit (double damage) {
+		lifebar = lifebar - damage;
+	}
+	private double lifebar;
+
+	public double Lifebar{
+		get
+		{
+			return lifebar;
+		}
+		set
+		{
+			lifebar = value;	
+		}
+	}
+	 	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
