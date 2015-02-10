afdf089f2249b16543d023ff1016cf623afaa18e
Caio Lins <csnrl@cin.ufpe.br>
2015-02-10 15:44:32 -0200

    augmented reality

---
+++ Assets/Scripts/ModelsController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ModelsController : MonoBehaviour {
+
+	public metaioTracker myMetaioTracker;
+	public metaioSDK myMetaioSDK;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// Demo # 1
+		if(Input.GetKeyDown("a"))
+		{
+			Debug.Log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+			//this.transform.localScale = new Vector3 (myMetaioTracker.cameraPosition.transform.rotation.eulerAngles.z / 100, myMetaioTracker.cameraPosition.transform.rotation.eulerAngles.z / 100, myMetaioTracker.cameraPosition.transform.rotation.eulerAngles.z / 100);
+
+		}
+	}
+}
