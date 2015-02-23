59c29f7d0f746ed613ea9eced29e07ca693d7eed
arthurmnd <arbmendonca@gmail.com>
2015-02-23 16:42:09 -0300

    Lifebar atualizada

--- Assets/Project/Scripts/Game/LifebarBehaviour.cs
+++ Assets/Project/Scripts/Game/LifebarBehaviour.cs
@@ -5,19 +5,30 @@ public class LifebarBehaviour : MonoBehaviour {
     CharacterBehaviour cb;
     private int life;
     TextMesh txt;
-	// Use this for initialization
+    Transform barra;
+    Vector3 barraSize;
+    Vector3 targetSize;
+    float lifeRatio;
+    // Use this for initialization
     void Awake () {
         Transform pai = transform.parent;
         cb = pai.GetComponent<CharacterBehaviour>();
         life = (int) Mathf.Ceil(cb.Lifebar);
         txt = this.GetComponent<TextMesh>();
+        barra = transform.Find("Bar");
+        barraSize = barra.localScale;
+        targetSize = barraSize;
+        targetSize.x = 0.0f;
     }
-	void Start () {
-
-	}
-	// Update is called once per frame
-	void Update () {
-        life = (int) Mathf.Ceil(cb.Lifebar);
-        txt.text = life.ToString();
-	}
+    void Start () {
+        
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        //life = (int) Mathf.Ceil(cb.Lifebar);
+        lifeRatio = cb.Lifebar * 0.001f;
+        barra.localScale = Vector3.Lerp (targetSize, barraSize, lifeRatio);
+        barra.renderer.material.color = Color.Lerp(Color.red, Color.green, lifeRatio);
+    }
 }
